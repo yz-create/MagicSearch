b7a3cfa1a2faa63f893119c8441d49dc850e486f
louisadj <louisa.djebroun@eleve.ensai.fr>
2025-09-12 15:47:55 +0000

    sspcloud conn

--- sspcloud.py
+++ sspcloud.py
@@ -3,22 +3,24 @@ import os
 
 token = os.getenv("API_TOKEN")
 
-url = "https://1lm.lab.sspcloud.fr/ollama/api/embed"
+url = "https://llm.lab.sspcloud.fr/ollama/api/embed"
 
 headers = {
-    "Authorization" : f"Bearer {token}",
+    "Authorization": f"Bearer {token}",
     "Content-type": "application/json"}
 
-def embedding (text:str):
+
+def embedding(text: str):
 
     data = {
-        "model": "bge-m3: latest",
+        "model": "bge-m3:latest",
         "input": text
     }
 
-    response = requests.post(url, healers=headers, json=data)
+    response = requests.post(url, headers=headers, json=data)
     json_response = response.json()
     # res = json_response[ "embeddings"]
     return json_response
 
-print(embedding("The sky is blue") ["embeddings"])
+
+print(embedding("The sky is blue")["embeddings"])
