d04d83d139d2e643c4b9118952675b2a32e3623a
Poulpy77 <loic.guepin@orange.fr>
2025-09-19 10:47:15 +0000

    more physical schema creation

--- test_loic.py
+++ test_loic.py
@@ -1,4 +1,5 @@
 import json
+from collections import Counter
 
 file = open("AtomicCards.json")
 data = json.loads(file.read())
@@ -7,12 +8,19 @@ data = data['data']
 
 b = 0
 c = 0
+fore = []
 for i in data:
     card = data[i][0]
+    for j in list(card.keys()):
+        fore.append(j)
     try:
-        print(card["leadershipSkills"])
+        # fore.append(card["power"])
+        print(card["relatedCards"])
+        """for j in list(card["purchaseUrls"].keys()):
+            fore.append(j)"""
         c += 1
-        if c > 9:
-            break
     except:
         b += 1
+
+print(Counter(fore))
+print(c)
