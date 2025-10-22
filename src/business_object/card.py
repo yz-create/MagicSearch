68844ad11ba0acb178dcea26b3cecdc51e06c37d
louisadj <louisa.djebroun@eleve.ensai.fr>
2025-10-22 08:43:47 +0000

    commentaires pour card

--- src/business_object/card.py
+++ src/business_object/card.py
@@ -3,7 +3,9 @@ class Card:
         self._id_card = id_card
         self._name = name
         self._embedded = embedded
-
+        # à rajouter tous les arguments qu'une carte contient (regarder le bdd de gdoc et ajouter tous ceux qui ne sont pas vides)
+# créer une fonction get à chaque attribut 
+# utiliser le lien pluot que l'image 
     def show_card(self) -> str:
         """
         Return the official (or informal) representation.
