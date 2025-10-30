5f1a696d87c18dcbb348493ba174243f0b740428
louisadj <louisa.djebroun@eleve.ensai.fr>
2025-10-30 09:06:31 +0000

    app.py prêt

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ from fastapi import FastAPI, HTTPException
 from fastapi.responses import RedirectResponse
 from pydantic import BaseModel
 
-from service.joueur_service import JoueurService
+from service.user_service import UserService
 from utils.log_init import initialiser_logs
 
 app = FastAPI(title="MagicSearch")
@@ -14,13 +14,13 @@ initialiser_logs("Webservice")
 
 user_service = UserService()
 
-
+# route vers la documentation 
 @app.get("/", include_in_schema=False)
 async def redirect_to_docs():
     """Redirect to the API documentation"""
     return RedirectResponse(url="/docs")
 
-
+# routes utilisateurs : get user et get user id
 @app.get("/user/", tags=["Users"])
 async def list_all_users():
     """Lister tous les users"""
@@ -28,79 +28,82 @@ async def list_all_users():
     list_users = user_service.list_all()
 
     liste_model = []
-    for joueur in list_users:
+    for user in list_users:
         liste_model.append(user)
 
     return liste_model
 
 
 @app.get("/user/{user_id}", tags=["Users"])
-async def joueur_par_id(user_id: int):
-    """Trouver un joueur à partir de son id"""
-    logging.info("Trouver un joueur à partir de son id")
+async def user_par_id(user_id: int):
+    """Trouver un user à partir de son id"""
+    logging.info("Trouver un user à partir de son id")
     return user_service.find_by_id(user_id)
 
 
-class JoueurModel(BaseModel):
-    """Définir un modèle Pydantic pour les Joueurs"""
+# librairie Pydantic BaseModel
+class userModel(BaseModel):
+    """
+    Définir un modèle Pydantic pour les users
+    Modèle Pydantic pour valider et documenter les 
+    objets utilisateur reçus en entrée ou envoyés en sortie.
+    """
 
     user_id: int | None = None  # Champ optionnel
     pseudo: str
     mdp: str
 
 
-########################################################
-
+# création d'un user 
 @app.post("/user/", tags=["Users"])
-async def create_user(j: JoueurModel):
+async def create_user(j: userModel):
     """Créer un user"""
     logging.info("Create user")
     if user_service.pseudo_deja_utilise(j.pseudo):
         raise HTTPException(status_code=404, detail="Pseudo déjà utilisé")
 
-    joueur = joueur_service.creer(j.pseudo, j.mdp, j.age, j.mail, j.fan_pokemon)
-    if not joueur:
-        raise HTTPException(status_code=404, detail="Erreur lors de la création du joueur")
+    user = user_service.creer(j.pseudo, j.mdp)
+    if not user:
+        raise HTTPException(status_code=404, detail="Erreur lors de la création du user")
 
-    return joueur
+    return user
 
+# modification d"un user
+@app.put("/user/{id_user}", tags=["users"])
+def modifier_user(id_user: int, j: userModel):
+    """Modifier un user"""
+    logging.info(f"Modifier user {id_user}")
+    user = user_service.trouver_par_id(id_user)
+    if not user:
+        raise HTTPException(status_code=404, detail="user non trouvé")
 
-@app.put("/joueur/{id_joueur}", tags=["Joueurs"])
-def modifier_joueur(id_joueur: int, j: JoueurModel):
-    """Modifier un joueur"""
-    logging.info("Modifier un joueur")
-    joueur = joueur_service.trouver_par_id(id_joueur)
-    if not joueur:
-        raise HTTPException(status_code=404, detail="Joueur non trouvé")
+    user.pseudo = j.pseudo
+    user.mdp = j.mdp
+    user = user_service.modifier(user)
+    if not user:
+        raise HTTPException(status_code=404, detail="Erreur lors de la modification du user")
 
-    joueur.pseudo = j.pseudo
-    joueur.mdp = j.mdp
-    joueur.age = j.age
-    joueur.mail = j.mail
-    joueur.fan_pokemon = j.fan_pokemon
-    joueur = joueur_service.modifier(joueur)
-    if not joueur:
-        raise HTTPException(status_code=404, detail="Erreur lors de la modification du joueur")
-
-    return f"Joueur {j.pseudo} modifié"
+    return f"user {j.pseudo} modifié"
 
 
-@app.delete("/joueur/{id_joueur}", tags=["Joueurs"])
-def supprimer_joueur(id_joueur: int):
-    """Supprimer un joueur"""
-    logging.info("Supprimer un joueur")
-    joueur = joueur_service.trouver_par_id(id_joueur)
-    if not joueur:
-        raise HTTPException(status_code=404, detail="Joueur non trouvé")
+# suppression d'un user
+@app.delete("/user/{id_user}", tags=["users"])
+def supprimer_user(id_user: int):
+    """Supprimer un user"""
+    logging.info(f"Supprimer un user {id_user}")
+    user = user_service.trouver_par_id(id_user)
+    if not user:
+        raise HTTPException(status_code=404, detail="user non trouvé")
 
-    joueur_service.supprimer(joueur)
-    return f"Joueur {joueur.pseudo} supprimé"
+    user_service.supprimer(user)
+    return f"user {user.pseudo} supprimé"
 
 
+# route simple pour tester l'api
 @app.get("/hello/{name}")
 async def hello_name(name: str):
     """Afficher Hello"""
-    logging.info("Afficher Hello")
+    logging.info(f"Afficher Hello {name}")
     return f"message : Hello {name}"
 
 
