1b3cb9c0659183c8c305651bd9402b6832c77650
bonninnoemie <noemie.bonnin@eleve.ensai.fr>
2025-11-04 09:12:31 +0000

    user dao create version 1

--- src/dao/user_dao.py
+++ src/dao/user_dao.py
@@ -1,5 +1,9 @@
-from db_connection import DBConnection
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+from utils.log_decorator import log
+from db_connection import DBConnection
+import logging
 
 class UserDao:
     """Communication between UserService and the DBConnexion"""
@@ -14,9 +18,53 @@ class UserDao:
         """Re"""
         pass
 
-    def create(self, username: str, email: str, password: str):
-        """Créer un nouvel utilisateur"""
-        pass
+    @log
+    def create(self, username: str, email: str, password: str) -> bool:
+        """
+        Crée un nouvel utilisateur (joueur) dans la base de données.
+
+        Parameters
+        ----------
+        username : str
+        Le nom d'utilisateur du joueur
+        email : str
+        L'adresse e-mail du joueur
+        password : str
+        Le mot de passe du joueur (idéalement déjà haché)
+
+        Returns
+        -------
+        created : bool
+        True si la création est un succès, False sinon
+        """
+        res = None
+
+        try:
+            with DBConnection().connection as connection:
+                with connection.cursor() as cursor:
+                    cursor.execute(
+                        """
+                        INSERT INTO joueur (pseudo, mail, mdp)
+                        VALUES (%(pseudo)s, %(mail)s, %(mdp)s)
+                        RETURNING user_id;
+                        """,
+                        {
+                            "username": username,
+                            "mail": email,
+                            "password": password,
+                        },
+                    )
+                    res = cursor.fetchone()
+
+        except Exception as e:
+            logging.error(f"Erreur lors de la création du joueur : {e}")
+
+        created = False
+        if res:
+            self.id_joueur = res["id_joueur"]
+            created = True
+
+        return created
 
     def delete(self, user_id: int):
         """Supprimer un utilisateur"""
