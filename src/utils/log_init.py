03fe531a0129515c95d74a1c0cdf649941d565d6
Ilonamg <ilona.martinez-gutmann@eleve.ensai.fr>
2025-11-04 13:23:06 +0000

    log_init_anglais

--- src/utils/log_init.py
+++ src/utils/log_init.py
@@ -4,20 +4,20 @@ import logging.config
 import yaml
 
 
-def initialiser_logs(nom):
-    """Initialiser les logs à partir du fichier de config"""
+def initialize_logs(name):
+    """Initialize logs from the config file"""
 
     # print current working directory
     # print(os.getcwd())
     # os.chdir('ENSAI-2A-projet-info-template')
 
-    # Création du dossier logs à la racine si non existant
+    # Create the logs folder at the root if it doesn't exist
     os.makedirs("logs", exist_ok=True)
 
-    stream = open("logging_config.yml", encoding="utf-8")
-    config = yaml.load(stream, Loader=yaml.FullLoader)
+    with open("logging_config.yml", encoding="utf-8") as stream:
+        config = yaml.load(stream, Loader=yaml.FullLoader)
     logging.config.dictConfig(config)
 
     logging.info("-" * 50)
-    logging.info(f"Lancement {nom}                           ")
+    logging.info(f"Starting {name}                           ")
     logging.info("-" * 50)
