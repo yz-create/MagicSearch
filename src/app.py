e9f054e3030ec703e277a496ab6ce844b257281b
Ilonamg <ilona.martinez-gutmann@eleve.ensai.fr>
2025-11-05 14:04:05 +0000

    changement de port

--- src/app.py
+++ src/app.py
@@ -304,6 +304,6 @@ async def hello_name(name: str):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=9876)
+    uvicorn.run(app, host="0.0.0.0", port=9877)
 
     logging.info("Arret du Webservice")
